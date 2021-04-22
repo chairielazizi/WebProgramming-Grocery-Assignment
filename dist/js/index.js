40ab9df09b97ceced45ea37366a0f03c37faca83
kisyok <kisoindran@gmail.com>
2021-04-22 13:01:25 +0800

    edit index

--- dist/js/index.js
+++ dist/js/index.js
@@ -1,7 +1,14 @@
 function checkForm(){
     var emailInput = document.getElementById("inputEmail");
     var passwordInput = document.getElementById("inputPassword");
+    var submmit = document.getElementById("submitForm");
     if (emailInput.checkValidity() && passwordInput.checkValidity()){
-        window.location.href = "homepage.html"; 
+        submmit.setAttribute("type", "reset");
+        if(document.getElementById("option1").checked == true){
+            window.location.href = "homepage.html";
+        }else{
+            window.location.href = "adminpage.html";
+    }
     }
 }
+
