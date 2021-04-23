9e2641317a8a97332d9f5da7e13c1549c734a47e
kisyok <kisoindran@gmail.com>
2021-04-23 20:06:37 +0800

    update

---
+++ dist/js/editprofile.js
@@ -0,0 +1,21 @@
+
+const imgDiv = document.querySelector('.profile-pic-div')
+const img = document.querySelector('#profilePic');
+const file = document.querySelector('#file');
+const editPic = document.querySelector('#editPic');
+
+
+file.addEventListener('change', function(){
+    const choosedFile = this.files[0];
+
+    if (choosedFile){
+        const reader = new FileReader();
+
+        reader.addEventListener('load', function(){
+            img.setAttribute('src',reader.result);
+        }
+        );
+       reader.readAsDataURL(choosedFile);
+    }
+}
+);
