7fbd6231092d3ebeaa5b1e3a2b9c005cf7956ac1
kisyok <kisoindran@gmail.com>
2021-06-03 20:05:45 +0800

    front-end validation

--- dist/js/signup.js
+++ dist/js/signup.js
@@ -8,6 +8,7 @@ const terms = document.getElementById("terms");
 const submmit = document.getElementById("submitForm");
 const male = document.getElementById("male");
 const female = document.getElementById("female");
+const error = document.getElementById("error");
 
 firstName.addEventListener('keyup', checkValid);
 lastName.addEventListener('keyup', checkValid);
@@ -20,22 +21,26 @@ terms.addEventListener('change', checkTerms);
 
 function checkValid(e) {
     if(e.target.value !== '') {
-        e.target.classList.add('is-valid')
-        e.target.classList.remove('is-invalid')
+        e.target.classList.add('is-valid');
+        e.target.classList.remove('is-invalid');
+        return true;
     } else {
-        e.target.classList.remove('is-valid')
-        e.target.classList.add('is-invalid')
+        e.target.classList.remove('is-valid');
+        e.target.classList.add('is-invalid');
+        return false;
     }
 }
 
 function checkEmail(e){
     var mailformat = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
     if(e.target.value.match(mailformat) && e.target.value !== '' && e.target.value.includes(".com")){
-        e.target.classList.add('is-valid')
-        e.target.classList.remove('is-invalid')
+        e.target.classList.add('is-valid');
+        e.target.classList.remove('is-invalid');
+        return true;
     }else {
-        e.target.classList.remove('is-valid')
-        e.target.classList.add('is-invalid')
+        e.target.classList.remove('is-valid');
+        e.target.classList.add('is-invalid');
+        return false;
     }
 
 }
@@ -45,70 +50,98 @@ function goodPassword(e){
     var upperCaseLetters = /[A-Z]/g;
     var numbers = /[0-9]/g;
     if(e.target.value.length < 8 || e.target.value.length > 10){
-        e.target.classList.remove('is-valid')
-        e.target.classList.add('is-invalid')
+        e.target.classList.remove('is-valid');
+        e.target.classList.add('is-invalid');
         return false;
     }else if(!e.target.value.match(upperCaseLetters)){
-        e.target.classList.remove('is-valid')
-        e.target.classList.add('is-invalid')
+        e.target.classList.remove('is-valid');
+        e.target.classList.add('is-invalid');
         return false;
     }else if(!e.target.value.match(lowerCaseLetters)){
-        e.target.classList.remove('is-valid')
-        e.target.classList.add('is-invalid')
+        e.target.classList.remove('is-valid');
+        e.target.classList.add('is-invalid');
         return false;
     }else if(!e.target.value.match(numbers)){
-        e.target.classList.remove('is-valid')
-        e.target.classList.add('is-invalid')
+        e.target.classList.remove('is-valid');
+        e.target.classList.add('is-invalid');
         return false;
     }else{
-        e.target.classList.add('is-valid')
-        e.target.classList.remove('is-invalid')
+        e.target.classList.add('is-valid');
+        e.target.classList.remove('is-invalid');
         return true;
     }
 }
 
 function matchPassword(e){
     if(passwordInput.value === e.target.value && e.target.value !== '' && goodPassword(e)){
-        e.target.classList.add('is-valid')
-        e.target.classList.remove('is-invalid')
+        e.target.classList.add('is-valid');
+        e.target.classList.remove('is-invalid');
         return true;
     }else{
-        e.target.classList.remove('is-valid')
-        e.target.classList.add('is-invalid')
+        e.target.classList.remove('is-valid');
+        e.target.classList.add('is-invalid');
         return false;
     }
 }
 
 function checkDate(e){
     if (!e.target.value) {
-        e.target.classList.remove('is-valid')
-        e.target.classList.add('is-invalid')
+        e.target.classList.remove('is-valid');
+        e.target.classList.add('is-invalid');
+        return false;
     }else{
-        e.target.classList.add('is-valid')
-        e.target.classList.remove('is-invalid')
+        e.target.classList.add('is-valid');
+        e.target.classList.remove('is-invalid');
+        return true;
     }
 }
 
 function checkTerms(e){
-        if(!e.target.checked){
-            e.target.classList.remove('is-valid')
-            e.target.classList.add('is-invalid')
-            return false;
-        }else{
-            e.target.classList.add('is-valid')
-            e.target.classList.remove('is-invalid')
-            return true;
-        }
+    if(!e.target.checked){
+        e.target.classList.remove('is-valid');
+        e.target.classList.add('is-invalid');
+        return false;
+    }else{
+        e.target.classList.add('is-valid');
+        e.target.classList.remove('is-invalid');
+        return true;
     }
+}
 
-// function checkForm(){
-//     if (firstName.checkValidity() && lastName.checkValidity() && emailInput.checkValidity() && passwordInput.checkValidity()
-//         && birthdate.checkValidity() && confirmPassword.checkValidity() && terms.checkValidity() && matchPassword()
-//         && goodPassword() && checkTerms()){
-//         submmit.setAttribute("type", "reset");
-//         window.location.href = "homepage.html";
-//     }
+// function validateForm(event){
+//     if(checkValid(firstName) == true && checkValid(lastName) == true && checkEmail(emailInput) == true 
+//         && goodPassword(passwordInput) == true && matchPassword(confirmPassword) ==true && 
+//         checkDate(birthdate) == true && checkTerms(terms) == true){
+//             return true;
+//         }else{
+            
+//             return false;
+//         }
 // }
+
+(function() {
+    'use strict';
+    window.addEventListener('load', function() {
+      // Get the forms we want to add validation styles to
+      var forms = document.getElementsByClassName('needs-validation');
+      // Loop over them and prevent submission
+      var validation = Array.prototype.filter.call(forms, function(form) {
+        form.addEventListener('submit', function(event) {
+          if (form.checkValidity() === false) {
+            event.preventDefault();
+            event.stopPropagation();
+          }
+          form.classList.add('was-validated');
+        }, false);
+      });
+    }, false);
+  })();
+
+
+
+
+
+
 
 
 
