98b150ebcbfeee6d35229d3f44594823e6e64cd9
kisyok <kisoindran@gmail.com>
2021-06-07 10:03:14 +0800

    update

--- processLogin.php
+++ processLogin.php
@@ -1,19 +1,37 @@
 <?php
 session_start();
-include_once("dist\php\connection.php");
+include_once("./dist/php/connection.php");
 
 if($_SERVER["REQUEST_METHOD"] = "POST"){
 
     if(isset($_POST["inputEmail"]) && isset($_POST["inputPassword"]) && isset($_POST["options"])){
-        $email = $_POST["inputEmail"];
+        $user_email = $_POST["inputEmail"];
         $password = $_POST["inputPassword"];
         $type = $_POST["options"];
         $hashedpassword = sha1($password);
         
         if($type == "admin"){
+            $sql = "SELECT * FROM adminaccount WHERE Email = '$user_email' AND Password = '$hashedpassword'";
+            $result = $conn->query($sql);
+            
+            if($result->num_rows > 0){
+                //if they are in the database, register the user in session
+                while($res = $result->fetch_assoc()){
+                    $userid = $res["user_id"];
+                    $email = $res["Email"];
+                }
 
+                //register session variables
+                $_SESSION['logged_in'] = true;
+                $_SESSION['user_id'] = $userid;
+                $_SESSION['user_email'] = $email;
+
+                header("Location: adminpage.html?action=login_success");
+            }else{
+                header("Location: index.php?action=login_failed");
+            }
         }else if($type == "customer"){
-            $sql = "SELECT * FROM account WHERE Email = '$email' AND Password = '$hashedpassword'";
+            $sql = "SELECT * FROM account WHERE Email = '$user_email' AND Password = '$hashedpassword'";
             $result = $conn->query($sql);
             
             if($result->num_rows > 0){
@@ -22,7 +40,6 @@ if($_SERVER["REQUEST_METHOD"] = "POST"){
                     $userid = $res["user_id"];
                     $name = $res["First name"];
                     $email = $res["Email"];
-                    echo "userid: " .$res['user_id']. " - Name: " .$res['First name']. " ". " - Email: ".$res['Email']. " - Type: ".$type. "<br>";
                 }
 
                 //register session variables
@@ -33,9 +50,7 @@ if($_SERVER["REQUEST_METHOD"] = "POST"){
 
                 header("Location: homepage.php?action=login_success");
             }else{
-                echo "no";
-                //invalid username or password
-                // header("Location: index.php?action=login_failed");
+                header("Location: index.php?action=login_failed");
             }
         }
         
