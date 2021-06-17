76edaee0133cf74445a8f2eaf3c0ed3609de4716
Mohamad Fairuz <mohamadfairuz1604@gmail.com>
2021-06-17 16:54:47 +0800

    removed files

--- homepage.php
+++ homepage.php
@@ -417,8 +417,6 @@ include_once './dist/php/connection.php';
   <?php  include_once './dist/php/retrievelist.php'; ?>
   <?php include_once './dist/php/homepagefunction.php'; ?>
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
-  <script src="dist/js/modal.js"></script>
-  
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" crossorigin="anonymous"></script>
 
 </body>
