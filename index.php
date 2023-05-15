36631716415373d1540ae93254f322db56af6784
Izzat Razab <55503571+izzatrazab@users.noreply.github.com>
2023-05-16 00:47:47 +0800

    login page: fix some css
    1) create all.css as universal css for all page
    2) fix height of the page by adding all.css

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
         <title>TroliMart Login Page</title>
           <meta charset="utf-8">
           <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+          <link rel="stylesheet" type="text/css" href="dist\css\all.css">
           <link rel="stylesheet" type="text/css" href="dist\css\index.css">
           <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
         <script type="text/javascript">
